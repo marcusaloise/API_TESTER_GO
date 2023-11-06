172f36f7d2847d7cab972825e1241099fb56c8f3
marcusaloise <marcusaloise@gmail.com>
2023-11-06 16:58:56 -0400

    Criando Hello, Word

---
+++ .go_ent/go.mod
@@ -0,0 +1,3 @@
+module github.com/marcusaloise/API_TESTER_GO
+
+go 1.21.3
---
+++ go/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// Removendo a maldição do GO
+func main() {
+	fmt.Println("Hello, Word!")
+}
